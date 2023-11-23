833a505abcdb8450339f9ccca0b57c4e02ac2b17
ABDULSAMED <samet.1198@gmail.com>
2023-11-23 12:45:36 +0300

    update 🆕: settings

--- lib/app/views/view_settings/settings_view.dart
+++ lib/app/views/view_settings/settings_view.dart
@@ -60,11 +60,14 @@ class SettingsView extends StatelessWidget {
                       ),
                       CustomListTile(
                         ontap: () {
-                          context.router.replace(const SignInViewRoute());
+                          // context.router.replace(const SignInViewRoute());
                         },
-                        leading: const Icon(Icons.logout_rounded),
-                        title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                        leading: const Icon(
+                          Icons.person_2_outlined,
+                          color: AppLightColorConstants.bgAccent,
+                        ),
+                        title: L10n.of(context)!.myProfile,
+                        trailing:  Icon(Icons.arrow_forward_ios_sharp,  color: AppLightColorConstants.greyteam1,),
                       ),
                       const Divider(
                         thickness: 1,
@@ -73,9 +76,12 @@ class SettingsView extends StatelessWidget {
                         ontap: () {
                           context.router.replace(const SignInViewRoute());
                         },
-                        leading: const Icon(Icons.logout_rounded),
-                        title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                        leading: const Icon(
+                          Icons.language_outlined,
+                          color: AppLightColorConstants.successColor,
+                        ),
+                        title: L10n.of(context)!.language,
+                        trailing:  Icon(Icons.arrow_forward_ios_sharp,  color: AppLightColorConstants.greyteam1,),
                       ),
                       const Divider(
                         thickness: 1,
@@ -84,9 +90,12 @@ class SettingsView extends StatelessWidget {
                         ontap: () {
                           context.router.replace(const SignInViewRoute());
                         },
-                        leading: const Icon(Icons.logout_rounded),
-                        title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                        leading: Icon(
+                          Icons.key_rounded,
+                          color: AppLightColorConstants.greyteam1,
+                        ),
+                        title: L10n.of(context)!.changePassword,
+                        trailing: Icon(Icons.arrow_forward_ios_sharp, color: AppLightColorConstants.greyteam1,),
                       ),
                       const Divider(
                         thickness: 1,
@@ -95,9 +104,9 @@ class SettingsView extends StatelessWidget {
                         ontap: () {
                           context.router.replace(const SignInViewRoute());
                         },
-                        leading: const Icon(Icons.logout_rounded),
-                        title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                        leading: const Icon(Icons.privacy_tip_outlined, color: AppLightColorConstants.bgAccent,),
+                        title: L10n.of(context)!.privacyPolicy,
+                        trailing:  Icon(Icons.arrow_forward_ios_sharp,  color: AppLightColorConstants.greyteam1,),
                       ),
                       const Divider(
                         thickness: 1,
@@ -106,72 +115,10 @@ class SettingsView extends StatelessWidget {
                         ontap: () {
                           context.router.replace(const SignInViewRoute());
                         },
-                        leading: const Icon(Icons.logout_rounded),
+                        leading: const Icon(Icons.logout_rounded, color: AppLightColorConstants.bgError,),
                         title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
+                        trailing:  Icon(Icons.arrow_forward_ios_sharp,  color: AppLightColorConstants.greyteam1,),
                       ),
-                      const Divider(
-                        thickness: 1,
-                      ),
-                      CustomListTile(
-                        ontap: () {
-                          context.router.replace(const SignInViewRoute());
-                        },
-                        leading: const Icon(Icons.logout_rounded),
-                        title: L10n.of(context)!.logout,
-                        trailing: const Icon(Icons.arrow_forward_ios_sharp),
-                      ),
-                      // CustomCard(
-                      //   prefixIcon: Icons.person,
-                      //   name: L10n.of(context)!.personalInfo,
-                      //   suffixIcon: Icons.arrow_forward_ios_rounded,
-                      //   iconColor: const Color(0xFF304EFE),
-                      //   shadowColor: const Color(0xFFeaebff),
-                      //   onPressed: () {
-                      //     // context.router.push(const UserInfoViewRoute());
-                      //   },
-                      // ),
-                      // CustomCard(
-                      //   prefixIcon: Icons.language,
-                      //   name: L10n.of(context)!.language,
-                      //   suffixIcon: Icons.arrow_forward_ios_rounded,
-                      //   iconColor: const Color(0xFF439f48),
-                      //   shadowColor: const Color(0xff4e9f5e9),
-                      //   onPressed: () {
-                      //     // context.router.push(const LanguageViewRoute());
-                      //   },
-                      // ),
-                      // CustomCard(
-                      //   prefixIcon: Icons.lock,
-                      //   name: L10n.of(context)!.changePassword,
-                      //   suffixIcon: Icons.arrow_forward_ios_rounded,
-                      //   iconColor: const Color(0xFF2a2a2a),
-                      //   shadowColor: const Color(0xFFb8b8b8),
-                      //   onPressed: () {
-                      //     // context.router.push(const PasswordViewRoute());
-                      //   },
-                      // ),
-                      // CustomCard(
-                      //   prefixIcon: Icons.article,
-                      //   name: L10n.of(context)!.privacyPolicy,
-                      //   suffixIcon: Icons.arrow_forward_ios_rounded,
-                      //   iconColor: const Color(0xFF304EFE),
-                      //   shadowColor: const Color(0xFFeaebff),
-                      //   onPressed: () {
-                      //     // context.router.push(const NotFoundViewRoute());
-                      //   },
-                      // ),
-                      //  CustomCard(
-                      //   prefixIcon: Icons.logout,
-                      //   name: L10n.of(context)!.logout,
-                      //   suffixIcon: Icons.arrow_forward_ios_rounded,
-                      //   iconColor: Color(0xFFfe3b30),
-                      //   shadowColor: Color(0xFFffebed),
-                      //   showDivider: false,
-                      //   onPressed: () {
-                      //     // context.router.push(const LoginViewRoute());
-                      //   },
-                      // )
                     ],
                   ),
                 ],
